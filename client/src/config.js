88cbde116834afb485a34361ca583b8c9bf7ee42
Rukh-Khan <khanrukhsar1890@gmail.com>
2024-11-26 08:08:08 +0530

    final-commit

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,6 @@
-const BACKEND_URL =
-  import.meta.env.NODE_ENV === "production"
-    ? import.meta.env.VITE_API_URL
-    : "http://localhost:5000";
+const BACKEND_URL = import.meta.env.PROD
+  ? import.meta.env.VITE_API_URL
+  : "http://localhost:5000";
 
 const config = {
   API_URL: BACKEND_URL,
